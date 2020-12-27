1e43c30ed09b4ad4e919239bb9b213ef04077f2b
DEVSOG12 <oreofesolarin@gmail.com>
2020-12-27 15:17:58 +0100

    Dome

--- SDG_api/server.js
+++ SDG_api/server.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults();
 const path = require('path');
 
 require('dotenv').config();
-
+// COmmit Error
 const port = process.env.PORT || 3000;
 const cors =require('cors');
 const bodyParser = require('body-parser');
